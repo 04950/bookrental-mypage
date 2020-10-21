a37089b87c9aeb02db97e324a0b4e725dabb00c5
04950 <yeonkyung.park@sk.com>
2020-10-21 18:03:35 +0900

    1st commit

--- src/main/java/bookrental/MypageRepository.java
+++ src/main/java/bookrental/MypageRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface MypageRepository extends CrudRepository<Mypage, Long> {
 
     List<Mypage> findByBookId(Long bookId);
-    List<Mypage> findByBookId(Long bookId);
+
 
         void deleteByAskId(Long askId);
 }
