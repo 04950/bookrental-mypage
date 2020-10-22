95f005f5446f7036c8299325739b2745a06ffe3d
04950 <yeonkyung.park@sk.com>
2020-10-22 09:30:40 +0900

    1차수정

--- src/main/java/bookrental/AskCanceled.java
+++ src/main/java/bookrental/AskCanceled.java
@@ -4,6 +4,7 @@ public class AskCanceled extends AbstractEvent {
 
     private Long id;
     private String status;
+    private Long bookId;
 
     public AskCanceled(){
         super();
@@ -16,6 +17,15 @@ public class AskCanceled extends AbstractEvent {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
     public String getStatus() {
         return status;
     }
